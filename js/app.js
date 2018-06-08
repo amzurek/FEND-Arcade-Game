9596333bdbce488839c965dfd65a051e1f77c52f
Arkadiusz Zurek <arkadiuszmarekzurek@gmail.com>
2018-06-09 00:06:16 +0200

    ADD: arcade game

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, fastRate) {
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // Variables for each enemy position and fastRate
+    this.x = x;
+    this.y = y;
+    this.fastRate = fastRate;
+
+    // The image/sprite for our enemies, this uses a helper we've provide to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+
+    // Multiplies any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
+    // all computers
+    this.x += this.fastRate * dt;
+
+    // Finding collisions between enemies and player
+    if(player.x < this.x + 80 && player.x + 80 > this.x && player.y < this.y + 50 && 50 + player.y > this.y) {
+        player.x = 200;
+        player.y = 400;
+    }
+
+    // Random speed for each enemy
+    if(this.x > 500) {
+        this.x = -100;
+        this.fastRate = 100 + Math.floor(Math.random() * 234);
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,26 +37,90 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player x and y starting position
+var playerX = 200;
+var playerY = 400;
+
+var Player = function() {
+    this.x = playerX;
+    this.y = playerY;
+    this.sprite = 'images/char-boy.png';
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+Player.prototype.update = function() {
 
+    //setting the x an y of game play space
+    if (this.x < 0) {
+        this.x = 0;
+    } else if(this.x > 400) {
+        this.x = 400;
+    }
+    else if(this.y > 400) {
+        this.y = 400;
+    }
 
+    //reset when player touch the water and change background color
+    else if (this.y < 0) {
+        setTimeout(function() {
+            player.x = 200;
+            player.y = 400;
+        }, 800);
+        changeColor('lightgreen');
+    }
+};
+
+// Changing background color when player wins
+function changeColor(colorOfWin) {
+    if(colorOfWin) {
+        document.getElementById("winner").style.backgroundColor = colorOfWin;
+        setTimeout(changeColor, 800);
+    } else {
+        document.getElementById("winner").style.backgroundColor = 'white';
+        return colorOfWin;
+    }
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Arrow keys to move player on the game space
+Player.prototype.handleInput = function(allowedKeys) {
+
+    if(allowedKeys === 'left' && this.x > 10) {
+        this.x -= 100;
+    }
+    if(allowedKeys === 'up' && this.y > 2) {
+        this.y -= 83;
+    }
+    if(allowedKeys === 'right' && this.x < 350) {
+            this.x += 100;
+    }
+    if(allowedKeys === 'down' && this.y < 400) {
+        this.y += 80;
+    }
+};
+
+// Starting position of player
+var player = new Player();
+
+// All enemies objects in an array
+var enemy1 = new Enemy(-100, 63, 80);
+var enemy2 = new Enemy(-200, 145, 90);
+var enemy3 = new Enemy(-300, 230, 60);
+
+var allEnemies = [enemy1, enemy2, enemy3];
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+// Player.handleInput() method.
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -9,7 +9,7 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine makes the canvas' context (ctx) object globally available to make 
+ * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
 
@@ -90,7 +90,7 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
+        allEnemies.forEach(function (enemy) {
             enemy.update(dt);
         });
         player.update();
@@ -117,9 +117,9 @@ var Engine = (function(global) {
             numRows = 6,
             numCols = 5,
             row, col;
-        
+
         // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height)
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
@@ -149,11 +149,12 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
+        allEnemies.forEach(function (enemy) {
             enemy.render();
         });
 
         player.render();
+
     }
 
     /* This function does nothing but it could have been a good place to
